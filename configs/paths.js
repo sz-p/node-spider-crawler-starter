039cf3b667bf11a319af74d9e9e3b54302455547
sz-p <sz_p@outlook.com>
2020-08-24 00:06:42 +0800

    [feat]: add downloadfile

--- configs/paths.js
+++ configs/paths.js
@@ -2,7 +2,15 @@ const path = require('path');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 export default {
-	errorFile_CSV: resolveApp('./logs/error.csv'),
-	successFile_CSV: resolveApp('./logs/success.csv'),
-	header: resolveApp('./configs/header.js')
+	errorFile_CSV: resolveApp('./downloadData/logs/error.csv'),
+	successFile_CSV: resolveApp('./downloadData/logs/success.csv'),
+	infoFile_CSV: resolveApp('./downloadData/logs/info.csv'),
+
+	header: resolveApp('./configs/header.js'),
+
+	downloaded: resolveApp('./downloadData/downloaded'),
+	waitList: resolveApp('./downloadData/waitList'),
+	errorList: resolveApp('./downloadData/errorList'),
+
+	downloadDataDir: resolveApp('./downloadData')
 };
